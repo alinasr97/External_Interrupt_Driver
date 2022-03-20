9acaf6c9fc8c2f1a8e261156ba8cb7000fbeb8d8
Ali Nasr <100489075+alinasr97@users.noreply.github.com>
2022-03-20 16:37:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -15,30 +15,40 @@
 
 int main(void)
 {
+	void func1(void)
+	{
+		DIO_SetPinValue(PORT_A,PIN0,HIGH);
+	}
+	
+	void func2(void)
+	{
+		DIO_SetPinValue(PORT_A,PIN0,LOW);
+	}
 	EXI_TriggerEdge(EX_INT0,FALLING_EDGE);
 	EXI_Enable(EX_INT0);
 	EXI_TriggerEdge(EX_INT1,FALLING_EDGE);
 	EXI_Enable(EX_INT1);
+	
+	EXI_SetCallBack(EX_INT0 , func1);
+	EXI_SetCallBack(EX_INT1 , func2);
 	sei(); // enable global interrupt
 	//led
 	DIO_SetPinDirection(PORT_A,PIN0,OUTPUT);
+	DIO_SetPinDirection(PORT_A,PIN7,OUTPUT);
 	//switch
 	DIO_SetPinDirection(PORT_D,PIN2,INPUT);
 	DIO_SetPinDirection(PORT_D,PIN3,INPUT);
 	DIO_SetPinValue(PORT_D,PIN2,HIGH);
 	DIO_SetPinValue(PORT_D,PIN3,HIGH);
+	
     while (1) 
     {
 		
-		
+		DIO_SetPinValue(PORT_A,PIN7,HIGH);
+		_delay_ms(50);
+		DIO_SetPinValue(PORT_A,PIN7,LOW);
+		_delay_ms(50);
     }
 	
 }
-ISR(INT0_vect)
-{
-	DIO_SetPinValue(PORT_A,PIN0,HIGH);
-}
-ISR(INT1_vect)
-{
-	DIO_SetPinValue(PORT_A,PIN0,LOW);
-}
+
